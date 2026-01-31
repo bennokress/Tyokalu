3407f94a2c32b4dcc1fbe6f8a40879ce8237eb7b
Benno Kress <developer+git-commit@bennokress.de>
2026-01-31 13:21:45 +0100

    Initial Commit

---
+++ Työkalu/TyökaluApp.swift
@@ -0,0 +1,17 @@
+//
+//  TyökaluApp.swift
+//  Työkalu
+//
+//  Created by Benno on 2026-01-31.
+//
+
+import SwiftUI
+
+@main
+struct TyökaluApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
